355be28ac7d92c4296ead1f16a0d4361926da675
DurgaKanth Dikkala <128021187+durgakanth@users.noreply.github.com>
2024-08-01 10:53:23 +0530

    Update animation.js

--- src/components/animation.js
+++ src/components/animation.js
@@ -1,20 +1,20 @@
-import Lottie from "lottie-react";
-import React from "react";
+// import Lottie from "lottie-react";
+// import React from "react";
 
-const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+// const AnimationLottie = ({ animationPath, width }) => {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationPath,
+//     style: {
+//       width: '95%',
+//     }
+//   };
 
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
-};
+//   return (
+//     <Lottie {...defaultOptions} />
+//   );
+// };
 
-export default AnimationLottie;
+// export default AnimationLottie;
